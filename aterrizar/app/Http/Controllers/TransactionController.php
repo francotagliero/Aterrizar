392fa804dd181dc1716414bc7273be44552f56bc
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-02 16:20:38 -0300

    tabla tansaction falta la vista

---
+++ aterrizar/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transactions;
+use Illuminate\Http\Request;
+use App\Transaction;
+
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+
+        $transactions = Transaction::all();
+
+        return view('transactions.index')->with('transactions', $transactions);
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTransaction $request) {
+
+        $transaction = new Transaction();
+        $transaction->service_name = $request->service_name; //deberia pasarse como parametro
+        $transaction->service_id = $request->service_id;
+        $transaction->user_id = $request->user_id;
+        $transaction->points = $request->points;
+        $transaction->points_given = 'false';
+        $transaction->price = $request->price;
+        $transaction->save();
+        return redirect('transactions');
+    }
+
+}
---
+++ aterrizar/app/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+	protected $fillable = [ 'service_name', 'service_id', 'user_id', 'points', 'points_given', 'price' ];
+
+
+	public function service($service) {
+
+		if($service=='Room'){
+			return $this->belongsTo('App\Room', 'service_name');
+		}   
+		else if($service=='Flight'){
+			return $this->belongsTo('App\Flight', 'service_name');
+		}
+		else{
+			return $this->belongsTo('App\Car', 'service_name');
+		}
+	}
+
+	public function user(){
+        return $this->belongsTo('App\User', 'id');
+    }
+
+
+}
