0b0bd3401b195b3d421b115c6402f4c8503d64ab
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-04 03:33:31 -0300

    borrado de tablas innesesarias, hay q agregar los campos al usuario y al registrar asignar el rol

--- aterrizar/resources/views/layouts/app.blade.php
+++ aterrizar/resources/views/layouts/app.blade.php
@@ -53,6 +53,9 @@
                       <li class="nav-item">
                       <a class="nav-link" href="#"> Mis Compras </a>
                       </li>
+                      <li class="nav-item">
+                      <a class="nav-link" href="#"> Mis Puntos </a>
+                      </li>
                        @elseif(Auth::user()->hasRole('admin'))
                        <li class="nav-item">
                          <a class="nav-link" href="../adminpanel"> Panel Configuracion</a>
