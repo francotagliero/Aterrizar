58fcac2028f70fb5ff29e1e2499e09f558cd40d1
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-06 19:34:03 -0300

    guest con busquedas

--- aterrizar/resources/views/layouts/app.blade.php
+++ aterrizar/resources/views/layouts/app.blade.php
@@ -42,16 +42,16 @@
               <a class="nav-link" href="flights"> Buscar Vuelos </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"> Buscar Autos </a>
+              <a class="nav-link" href="cars"> Buscar Autos </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#"> Buscar Hoteles </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="miCarrito"> Mi Carrito </a>
+              <a class="nav-link" href="#"> Mi Carrito </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="myShopping"> Mis Compras </a>
+              <a class="nav-link" href="#"> Mis Compras </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/myprofile"> Mi Perfil </a>
@@ -87,11 +87,11 @@
         <li class="nav-item">
           <a class="nav-link" href="../../rooms/create"> Agregar Habitacion </a>
         </li>
-         @if (empty(Auth::user()->dni))
-          <li class="nav-item">
-            <a class="nav-link" href="/myprofile"> <mark> Agregar Información Faltante </mark> </a>
-          </li>
-          @endif
+        @if (empty(Auth::user()->dni))
+        <li class="nav-item">
+          <a class="nav-link" href="/myprofile"> <mark> Agregar Información Faltante </mark> </a>
+        </li>
+        @endif
         <li class="nav-item">
           <a class="nav-link" href="/myprofile"> Mi Perfil </a>
         </li>
@@ -104,35 +104,43 @@
       <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->
         @guest
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-        </li>
-        <li class="nav-item">
-          @if (Route::has('register'))
-          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-          @endif
-        </li>
-        @else
-        <li class="nav-item dropdown">
-          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }} <span class="caret"></span>
-          </a>
-
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-            document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-          </a>
-
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-          </form>
-        </div>
+        <a class="nav-link" href="flights"> Buscar Vuelos </a>
       </li>
-      @endguest
-    </ul>
-  </div>
+      <li class="nav-item">
+        <a class="nav-link" href="cars"> Buscar Autos </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Buscar Hoteles </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+      </li>
+      <li class="nav-item">
+        @if (Route::has('register'))
+        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        @endif
+      </li>
+      @else
+      <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+          {{ Auth::user()->name }} <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+      </div>
+    </li>
+    @endguest
+  </ul>
+</div>
 </div>
 </nav>
 
