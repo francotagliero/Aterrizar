3209718b233e12565657bc2768055a959c73d9ff
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-07 14:03:26 -0300

    seeder room

---
+++ aterrizar/database/seeds/RoomsTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\{Room,Hotel};
+use Carbon\Carbon;
+
+
+
+class RoomsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('rooms')->truncate();
+        $romaHotel= Hotel::where('name', 'Roma')->first()->id;
+        $italiaHotel= Hotel::where('name', 'Italia')->first()->id;
+        $sudestadaHotel= Hotel::where('name', 'Sudestada')->first()->id;
+        $rooms = [
+            ['hotel_id' => $romaHotel,
+             'capacity' => 5,
+             'from' => new Carbon('2019-03-19'),
+             'to' => new Carbon('2019-03-30')],
+             ['hotel_id' => $italiaHotel,
+             'capacity' => 3,
+             'from' => new Carbon('2019-03-19'),
+             'to' => new Carbon('2019-03-30')],
+             ['hotel_id' => $sudestadaHotel,
+             'capacity' => 3,
+             'from' => new Carbon('2019-03-20'),
+             'to' => new Carbon('2019-03-25')],
+             ];
+         foreach ($rooms as $room) {
+            Room::create($room);
+        }
+
+
+
+    }
+}
