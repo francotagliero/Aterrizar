92ebce80d72f83aefb18d13a339726b03cdd9276
Nicolás Urquiola <nicourquiola@gmail.com>
2018-12-09 19:15:52 -0300

    Deshacer comentario

--- aterrizar/app/Services/SearchService.php
+++ aterrizar/app/Services/SearchService.php
@@ -107,20 +107,20 @@ class SearchService {
     }
 
 public function rooms($city, $capacity, $from, $to, $amenities) {
-    // $rooms = Room::join('hotels', 'hotels.id', '=', 'rooms.hotel_id')
-    // ->where([['capacity', '>=', $capacity],['city_id', '=', $city],['rooms.from', '>=', $from]])->get()->all();
-    // if(isset($amenities)){
-    // return array_filter($rooms, function ($room) use ($amenities) {
-    //     $roomAmenities = $room->hotel->amenities;
-    //     foreach ($amenities as $amenity) {
-    //         if (! in_array($amenity, $roomAmenities)) {
-    //             return false;
-    //         }
-    //         return true;
-    //     });}
-    //     else{
-    //         return $rooms;
-    //     }
+    $rooms = Room::join('hotels', 'hotels.id', '=', 'rooms.hotel_id')
+    ->where([['capacity', '>=', $capacity],['city_id', '=', $city],['rooms.from', '>=', $from]])->get()->all();
+    if(isset($amenities)){
+    return array_filter($rooms, function ($room) use ($amenities) {
+        $roomAmenities = $room->hotel->amenities;
+        foreach ($amenities as $amenity) {
+            if (! in_array($amenity, $roomAmenities)) {
+                return false;
+            }
+            return true;
+        });}
+        else{
+            return $rooms;
+        }
     }
 
     public function cars($from, $to, $date_rent, $date_return, $brand, $agency) {
