ef09f96804eab5a40b54d144cb9e109465d02b3b
Nicolás Urquiola <nicourquiola@gmail.com>
2018-12-09 20:25:15 -0300

    Borrar comentario

--- aterrizar/app/Transaction.php
+++ aterrizar/app/Transaction.php
@@ -9,18 +9,7 @@ class Transaction extends Model
 	protected $fillable = [ 'service_type', 'service_id', 'user_id', 'points', 'points_given', 'price', 'status', 'from', 'to', 'extra'];
 
     protected $casts = [ 'extra' => 'json' ];
-
-	// public function service($service) {
-	// 	if($service=='Room'){
-	// 		return $this->belongsTo('App\Room', 'service_name');
-	// 	}   
-	// 	else if($service=='Flight'){
-	// 		return $this->belongsTo('App\Flight', 'service_name');
-	// 	}
-	// 	else{
-	// 		return $this->belongsTo('App\Car', 'service_name');
-	// 	}
-    // }
+    
     
     public function service() {
 
