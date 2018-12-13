8486fc1717c6a2c352cfd7dfdfb6c7a521cb5e05
Nicolás Urquiola <nicourquiola@gmail.com>
2018-12-13 00:10:05 -0300

    Actualizar vista de carrito

--- aterrizar/resources/views/myCart/index.blade.php
+++ aterrizar/resources/views/myCart/index.blade.php
@@ -27,21 +27,47 @@
                             <td><a href="{!! route('flights.show', $transaction->service_id) !!}">
                             {{ $transaction->service->from->name }} &gt; {{ $transaction->service->to->name }}
                             </a>
+                            ({{ Carbon\Carbon::parse($transaction->service->date)->format('d-m-Y') }})
                             @if ($transaction->detail->stop !== null)
                             <br>
                             <a href="{!! route('flights.show', $transaction->detail->stop->id) !!}">
                             {{ $transaction->detail->stop->from->name }} &gt; {{ $transaction->detail->stop->to->name }}
                             </a>
+                            ({{ Carbon\Carbon::parse($transaction->detail->stop->date)->format('d-m-Y') }})
                             @endif
+                            <br>
+                            @switch($transaction->detail->class)
+                                @case('Economy')
+                                Clase Económica
+                                @break
+                                @case('Business')
+                                Clase Ejecutiva
+                                @break
+                                @case('First')
+                                Primera Clase
+                            @endswitch
                             </td>
                             @break
                         @case('Car')
                             <td>Auto</td>
-                            <td>Detalles del auto</td>
+                            <td><a href="{!! route('cars.show', $transaction->service_id) !!}">
+                            {{ $transaction->service->brand->name }} {{ $transaction->service->model }}
+                            </a>
+                            <br>
+                            {{ $transaction->service->agency->name }}
+                            <br>
+                            Del {{ Carbon\Carbon::parse($transaction->service->from)->format('d-m-Y') }} al {{ Carbon\Carbon::parse($transaction->service->to)->format('d-m-Y') }}
+                            </td>
                             @break
                          @case('Room')
                             <td>Habitación</td>
-                            <td>Detalles de la habitación</td>
+                            <td><a href="{!! route('rooms.show', $transaction->service_id) !!}">
+                            Hotel {{ $transaction->service->hotel->name }} 
+                            </a>
+                            ({{ $transaction->service->capacity }} personas)
+                            <br>
+                            Del {{ Carbon\Carbon::parse($transaction->service->from)->format('d-m-Y') }} al {{ Carbon\Carbon::parse($transaction->service->to)->format('d-m-Y') }}
+                            </td>
                     @endswitch
                     <td>{{ $transaction->points }}</td>
                     <td>{{ $transaction->points_given ? 'Sí' : 'No' }}</td>
