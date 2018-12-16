3ccfc182836e53d6e2a90e244588a20e7e579e67
Nicolás Urquiola <nicourquiola@gmail.com>
2018-12-16 02:37:42 -0300

    Actualizar plantilla de alertas

--- aterrizar/resources/views/common/alert.blade.php
+++ aterrizar/resources/views/common/alert.blade.php
@@ -1,4 +1,17 @@
 
-<div class="alert alert-{{ $type }} col-sm-12" role="alert">
+<div class="alert alert-{{ $type }} col-auto mr-auto ml-auto" role="alert">
+@switch($type)
+    @case('success')
+        <i class="fas fa-check-circle fa-lg"></i>
+        @break
+    @case('danger')
+        <i class="fas fa-exclamation-circle fa-lg"></i>
+        @break
+    @case('warning')
+        <i class="fas fa-exclamation-triangle fa-lg"></i>
+        @break
+    @case('info')
+        <i class="fas fa-info-circle fa-lg"></i>
+@endswitch
 {{ $message }}
 </div>
