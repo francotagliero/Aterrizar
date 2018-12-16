f42bcc51a0a593b54a7257733158fb91d964edb4
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-16 18:01:04 -0300

    actualizar panel de admin

--- aterrizar/database/seeds/AdminPanelsTableSeeder.php
+++ aterrizar/database/seeds/AdminPanelsTableSeeder.php
@@ -14,8 +14,8 @@ class AdminPanelsTableSeeder extends Seeder
     {    
         DB::table('admin_panel')->truncate();
         $adminpanel = new AdminPanel();
-        $adminpanel->max_flight_duration = 10;
-        $adminpanel->max_gap = 2;
+        $adminpanel->max_flight_duration = 30;
+        $adminpanel->max_gap = 4;
         $adminpanel->percentage_stopover = 0.50;
         $adminpanel->return_tax = 0.10;
        	$adminpanel->points_per_peso = 0.75;
