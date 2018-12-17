9e22773aa6895d9f26f108d367001d3e96a86ce1
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-17 00:22:37 -0300

    portada admin comercial

--- aterrizar/resources/views/home.blade.php
+++ aterrizar/resources/views/home.blade.php
@@ -2,22 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    @auth
+    @include('common.title', ['title' => Auth::user()->name . ', bienvenido a Aterrizar'])
+    @endauth
+    @guest
+    @include('common.title', ['title' => 'Bienvenido a Aterrizar'])
+    @endguest
 @endsection
