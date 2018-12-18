a296f0a5197b057f82cbcde1532ca16288ec93dc
JOAQUIN PEREZ LEIBOVICH <joaquinperezleibovich@hotmail.com>
2018-12-18 13:45:12 -0300

    login sin olvide pass

--- aterrizar/resources/views/auth/login.blade.php
+++ aterrizar/resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recordar') }}
                                     </label>
                                 </div>
                             </div>
@@ -56,10 +56,6 @@
                                 <button type="submit" class="btn btn-primary" name="login">
                                     {{ __('Login') }}
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
                             </div>
                         </div>
                     </form>
